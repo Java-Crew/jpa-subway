11fc523f1598ee3277423475c09d1dee5971351b
pjy1368 <basejin0810@naver.com>
2021-12-01 12:28:01 +0900

    feat: JPA 기본 세팅 및 Station 엔티티화

--- src/main/java/com/javacrew/subway/domain/Station.java
+++ src/main/java/com/javacrew/subway/domain/Station.java
@@ -1,7 +1,17 @@
 package com.javacrew.subway.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Station {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
---
+++ src/main/java/com/javacrew/subway/repository/StationRepository.java
@@ -0,0 +1,8 @@
+package com.javacrew.subway.repository;
+
+import com.javacrew.subway.domain.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StationRepository extends JpaRepository<Station, Long> {
+    Station findByName(String name);
+}
