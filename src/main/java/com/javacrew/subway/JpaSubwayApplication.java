ae34ac5a9b200f62b34798c808df704babb9e44c
pjy1368 <basejin0810@naver.com>
2021-12-01 09:15:03 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/javacrew/subway/JpaSubwayApplication.java
@@ -0,0 +1,13 @@
+package com.javacrew.subway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSubwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSubwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javacrew/subway/JpaSubwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacrew.subway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSubwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
