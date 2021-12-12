c1f898baedd056c05446017a46879f6355333ce9
highright96 <nsw5296@naver.com>
2021-12-13 00:56:30 +0900

    feat: 역,노선 일대일 양방향 매핑 추가

---
+++ src/main/java/com/javacrew/subway/domain/LineStation.java
@@ -0,0 +1,29 @@
+package com.javacrew.subway.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LineStation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "line_id")
+    private Line line;
+
+    @OneToOne(mappedBy = "lineStation")
+    private NewStation newStation;
+}
---
+++ src/main/java/com/javacrew/subway/domain/NewStation.java
@@ -0,0 +1,29 @@
+package com.javacrew.subway.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NewStation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "line_station_id")
+    private LineStation lineStation;
+}
